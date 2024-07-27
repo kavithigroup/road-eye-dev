3554d6ea2bd702bdb605514e55cf2a39413d8e5f
Kavindu Pasan Kavithilaka <kavithipublications@gmail.com>
2024-07-27 12:08:28 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {PublicComponent} from "./components/public/public.component";
+import {NotFoundComponent} from "./components/public/not-found/not-found.component";
+import {LoginComponent} from "./components/public/login/login.component";
+import {SignUpComponent} from "./components/public/sign-up/sign-up.component";
+import {MainComponent} from "./components/main/main.component";
+import {DashboardComponent} from "./components/main/dashboard/dashboard.component";
+import {ReportsComponent} from "./components/main/reports/reports.component";
+import {VehiclesComponent} from "./components/main/vehicles/vehicles.component";
+import {MapComponent} from "./components/main/map/map.component";
+import {AccountComponent} from "./components/main/account/account.component";
+import {OrdersComponent} from "./components/main/orders/orders.component";
 
 const routes: Routes = [
-  {path: "", component: PublicComponent}
+  {
+    path: "", component: MainComponent, children: [
+      {path: "dashboard", component: DashboardComponent},
+      {path: "map", component: MapComponent},
+      {path: "vehicles", component: VehiclesComponent},
+      {path: "reports", component: ReportsComponent},
+      {path: "account", component: AccountComponent},
+      {path: "orders", component: OrdersComponent}
+    ]
+  },
+  {
+    path: "", component: PublicComponent, children: [
+      {path: "login", component: LoginComponent},
+      {path: "sign-up", component: SignUpComponent},
+      {path: "**", component: NotFoundComponent}
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,75 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PublicComponent } from './components/public/public.component';
-import { FooterComponent } from './components/common/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PublicComponent} from './components/public/public.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NotFoundComponent} from './components/public/not-found/not-found.component';
+import {LoginComponent} from './components/public/login/login.component';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {SignUpComponent} from './components/public/sign-up/sign-up.component';
+import {MatButtonModule} from "@angular/material/button";
+import {TermsComponent} from './components/public/terms/terms.component';
+import {MatRadioModule} from "@angular/material/radio";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MainComponent} from './components/main/main.component';
+import { DashboardComponent } from './components/main/dashboard/dashboard.component';
+import { MapComponent } from './components/main/map/map.component';
+import { VehiclesComponent } from './components/main/vehicles/vehicles.component';
+import { ReportsComponent } from './components/main/reports/reports.component';
+import { AccountComponent } from './components/main/account/account.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatBadgeModule} from "@angular/material/badge";
+import {GoogleMapsModule} from "@angular/google-maps";
+import { AddVehicleComponent } from './components/main/vehicles/add-vehicle/add-vehicle.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatSelectModule} from "@angular/material/select";
+import {MatListModule} from "@angular/material/list";
+import { LinkDeviceComponent } from './components/main/vehicles/link-device/link-device.component';
+import {QRCodeModule} from "angularx-qrcode";
+import { OrdersComponent } from './components/main/orders/orders.component';
 @NgModule({
   declarations: [
     AppComponent,
     PublicComponent,
-    FooterComponent
+    NotFoundComponent,
+    LoginComponent,
+    SignUpComponent,
+    TermsComponent,
+    MainComponent,
+    DashboardComponent,
+    MapComponent,
+    VehiclesComponent,
+    ReportsComponent,
+    AccountComponent,
+    AddVehicleComponent,
+    LinkDeviceComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    MatBadgeModule,
+    GoogleMapsModule,
+    MatDialogModule,
+    FormsModule,
+    MatSelectModule,
+    MatListModule,
+    QRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

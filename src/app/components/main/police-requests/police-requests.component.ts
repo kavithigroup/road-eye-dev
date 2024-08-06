b258005d2748c63aeec81dc107594b93535ac795
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2024-08-06 13:01:11 +0530

    created police new request

--- src/app/components/main/police-requests/police-requests.component.ts
+++ src/app/components/main/police-requests/police-requests.component.ts
@@ -1,4 +1,24 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+
+
+export interface Request {
+  id: number;
+  RequestTime: string;
+  RequestDate: string;
+  VehicleId: string;
+  Registration: string;
+  status: string;
+
+}
+
+const REQUEST_DATA: Request[] = [
+  { id: 1, RequestTime: '14:30', RequestDate: '2024-07-27', VehicleId: 'ABC123', Registration: 'Registed', status: 'Investigating' },
+  { id: 2, RequestTime: '15:00', RequestDate: '2024-07-27', VehicleId: 'DAC452', Registration: 'Unregisted', status: 'Complete' },
+  { id: 3, RequestTime: '16:15', RequestDate: '2024-07-27', VehicleId: 'AEQ243', Registration: 'Registed', status: 'Complete' }
+];
+
 
 @Component({
   selector: 'app-police-requests',
@@ -7,4 +27,10 @@ import { Component } from '@angular/core';
 })
 export class PoliceRequestsComponent {
 
+  displayedColumns: string[] = ['id', 'requestTime', 'requestDate', 'vehicleId', 'registration', 'status', 'action'];
+  dataSource = REQUEST_DATA;
+
 }
+
+
+

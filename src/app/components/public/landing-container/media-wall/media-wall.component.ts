f8dcde5b1cd6a45eb18df5efa087b435c2525952
kavithma <kavithmadilmanie@gmail.com>
2024-11-05 16:12:48 +0530

    Media Wall componenet

---
+++ src/app/components/public/landing-container/media-wall/media-wall.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-wall',
+  templateUrl: './media-wall.component.html',
+  styleUrls: ['./media-wall.component.sass']
+})
+export class MediaWallComponent {
+
+}

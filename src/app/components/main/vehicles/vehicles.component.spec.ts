897dbb09a6231ceb14dba8f85abd1f67d410a021
dyonwalter <135725036+dyonwalter@users.noreply.github.com>
2024-11-26 14:04:58 +0530

    Update vehicles.component.spec.ts

--- src/app/components/main/vehicles/vehicles.component.spec.ts
+++ src/app/components/main/vehicles/vehicles.component.spec.ts
@@ -2,12 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VehiclesComponent } from './vehicles.component';
 
+describe('VehiclesComponent', () => {
+  let component: VehiclesComponent;
+  let fixture: ComponentFixture<VehiclesComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [VehiclesComponent]
     });
-
+    fixture = TestBed.createComponent(VehiclesComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 

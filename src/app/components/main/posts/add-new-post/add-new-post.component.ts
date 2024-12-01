f2bb9cdde0658d6d466b836911e2205ef2a51491
kavithma <kavithmadilmanie@gmail.com>
2024-12-01 12:44:18 +0530

    example post request

--- src/app/components/main/posts/add-new-post/add-new-post.component.ts
+++ src/app/components/main/posts/add-new-post/add-new-post.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ApiService} from "../../../../services/api.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-new-post',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-post.component.sass']
 })
 export class AddNewPostComponent {
+  form = new FormGroup({
+    title : new FormControl(),
+    description : new FormControl(),
 
+  })
+constructor( private api: ApiService, private snackBar: MatSnackBar, private router : Router, private route : Router ){
+
+}
+
+  submit() {
+    this.form.updateValueAndValidity()
+    if (this.form.invalid) {
+      this.snackBar.open("Invalid Input: " , "close", {duration: 5000})
+      return;
+    }
+    this.api.post("/post/create", this.form.value).subscribe(httpResponse => {
+      if(httpResponse.body === "ok"){
+        this.router.navigateByUrl("/post").then()
+      }else{
+        this.snackBar.open( httpResponse.body, "close", {duration: 5000})
+      }
+    }, error => {
+
+    })
+
+
+  }
 }

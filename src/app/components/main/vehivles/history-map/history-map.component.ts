2cabc7acde4106fc06e50efd513b2f7845f64de8
kavithma <kavithmadilmanie@gmail.com>
2024-12-03 12:47:41 +0530

    hovering card map

--- src/app/components/main/vehivles/history-map/history-map.component.ts
+++ src/app/components/main/vehivles/history-map/history-map.component.ts
@@ -20,19 +20,34 @@ export class HistoryMapComponent implements AfterViewInit {
     strokeWeight: 2          // Weight (thickness)
   };
   @ViewChild("map") map!: GoogleMap
+  protected vehicleDetails: any;
 
   constructor(private route: ActivatedRoute, private api: ApiService) {
   }
 
   ngAfterViewInit() {
     this.route.params.subscribe(params => {
+
+      const vehicleId = params["id"];
+
+
       this.api.get("/vehicle/location-history/" + params["id"]).subscribe(httpRepsonse => {
         this.points = httpRepsonse.map((u: any) => ({lat: u.lat, lng: u.lon}))
         this.map.panTo({
           lat: this.points.map(u => u.lat).reduce((sum: number, value: number) => sum + value, 0) / this.points.length,
           lng: this.points.map(u => u.lng).reduce((sum: number, value: number) => sum + value, 0) / this.points.length
         })
-      })
+
+        // Second API call: Fetch vehicle details
+        this.api.post("/vehicle/details", { id: vehicleId }).subscribe(vehicleResponse => {
+          this.vehicleDetails = vehicleResponse.body; // Store vehicle details as needed
+          console.log(this.vehicleDetails); // For debugging
+        }, error => {
+          console.error('Error fetching vehicle details:', error);
+        });
+      }, error => {
+        console.error('Error fetching location history:', error);
+      });
     })
   }
 }

625dc20f33acfeef98280ac6454987ec8d9c7d75
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2024-12-03 23:34:06 +0530

    update the compoents uis

--- src/app/components/main/police-requests/police-view-request/police-location-history/police-location-history.component.ts
+++ src/app/components/main/police-requests/police-view-request/police-location-history/police-location-history.component.ts
@@ -14,10 +14,10 @@ export interface Request {
 
 }
 const REQUEST_DATA: Request[] = [
-  { Date: '2024-07-27', Time: '14:30',Location: "colombo" },
-  { Date: '2024-07-28', Time: '17:30',Location: "hnugegoda" },
-  { Date: '2024-07-29', Time: '13:30',Location: "homagama" },
-  { Date: '2024-07-230', Time: '19:30',Location: "avissawella" },
+  { Date: '2024-08-27', Time: '16:32',Location: "avissawella" },
+  { Date: '2024-07-30', Time: '15:54',Location: "homagama" },
+  { Date: '2024-07-16', Time: '15:20',Location: "maharagama" },
+  { Date: '2024-07-14', Time: '14:30',Location: "nugegoda" },
 
 ];
 
